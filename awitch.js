597c7031b8b0c59a2c127d5851921fb7c18b1906
ahsaim04 <ahsaim04@gmail.com>
2025-10-02 15:52:35 +0600

    init

---
+++ awitch.js
@@ -0,0 +1,16 @@
+ let fruit = "banana";
+ 
+ switch (fruit) {
+    case "apple":
+      console.log("Apple is red");
+      break;
+    case "banana":
+      console.log("Banana is yellow");
+      break;
+    case "orange":
+      console.log("Orange is orange");
+      break;
+    default:
+      console.log("Unknown fruit");
+  }
+
